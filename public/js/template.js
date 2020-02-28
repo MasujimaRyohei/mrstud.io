dbf82c8211a5ddf12df0b021d900e9888597ef5e
MasujimaRyohei <info@masujimaryohei.jp>
2020-02-28 20:25:37 +0900

    Add footer.

--- public/js/template.js
+++ public/js/template.js
@@ -1,5 +1,3 @@
 
-function footer() {
-    var h = '<div class="footer" style="width:100%; text-align:center;  position: absolute; bottom: 0;">&copy; 2020 MasujimaRyohei.</div>';
-    document.write(h);
-}
+var h = '<div class="footer" style="width:100%; text-align:center;  position: absolute; bottom: 0;">&copy; 2020 MasujimaRyohei.</div>';
+document.write(h);
